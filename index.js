222c95ea43351d88bf04efeba7850de38f391d51
Anmol Anand <anmolanand2k15@gmail.com>
2022-06-10 09:36:51 +0530

    Added Functions

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const say = require("./src/say/say");
 const add = require("./src/maths/add");
+const diff = require("./src/maths/diff");
+const mul = require("./src/maths/mul");
+const div = require("./src/maths/div");
 
 module.exports.say = say;
 module.exports.add = add;
+module.exports.diff = diff;
+module.exports.mul = mul;
+module.exports.div = div;
